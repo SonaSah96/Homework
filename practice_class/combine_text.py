f31c56d630199395eb8ed33a6dd0b640c5e94ece
Sonasah96 <SonaSah1919@gmail.com>
2020-12-21 16:56:14 +0400

    classwork1

---
+++ practice_class/combine_text.py
@@ -0,0 +1,18 @@
+import os
+
+def combine_all_textes():
+    abs_path = os.getcwd()
+    with open(os.path.join(abs_path, "combine.txt"), "w") as f:
+        for root, directories, files in os.walk("."):
+            print(root)
+            for name in files:
+                if name.endswith(".txt"):
+                    #if root != ".":
+                        #name = root[2:] + "/" + name
+                    with open(os.path.normpath(f'{root}/{name}'), 'r') as rf:
+                        context = rf.read()
+                        f.write(context)
+
+
+
+combine_all_textes()
